2f4e268f79fe7eaee50f7e3ebe08d64272e1604f
KBM106 <96017088+kbm-106@users.noreply.github.com>
2023-02-03 22:33:22 +0100

    html, javascript clean on login
    jai fait la base de la page il manque le css sinon C² et juste les requet api

--- LoginOrSignUp.IDE/LoginOrSignUp.js
+++ LoginOrSignUp.IDE/LoginOrSignUp.js
@@ -1,8 +1,44 @@
 function changeStyle() {
-    var link = document.getElementsByTagName("link")[0];
-    if (link.getAttribute("href") == "LoginOrSignUp1.css") {
-      link.setAttribute("href", "LoginOrSignUp2.css");
-    } else {
-      link.setAttribute("href", "LoginOrSignUp1.css");
+  var link = document.getElementsByTagName("link")[0];
+  if (link.getAttribute("href") == "LoginOrSignUp1.css") {
+      document.body.classList.add("fade-out");
+      setTimeout(() => {
+          link.setAttribute("href", "LoginOrSignUp2.css");
+          document.body.classList.remove("fade-out");
+      }, 500);
+  } else {
+      document.body.classList.add("fade-out");
+      setTimeout(() => {
+          link.setAttribute("href", "LoginOrSignUp1.css");
+          document.body.classList.remove("fade-out");
+      }, 500);
+  }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const icon = document.getElementById("icon");
+    icon.addEventListener("click", function() {
+        if(icon.getAttribute("name") === "moon-outline") {
+            setTimeout(() => {
+                icon.setAttribute("name", "sunny-outline");
+            }, 500);
+        } else {
+            setTimeout(() => {
+                icon.setAttribute("name", "moon-outline");
+            }, 500);
+        }
+    });
+});
+
+function BP_1() { // fonction appele par le boutton 
+    if(document.getElementById('2').style.display == "block"){ //lorsque que tu click, si on voit la div
+    document.getElementById('2').style.display = "none";// tu la cache
+    document.getElementById('1').style.display = "block";
     }
+      else{
+      document.getElementById('2').style.display = "block";// affiche la div
+      document.getElementById('1').style.display = "none";
+      }       
   }
