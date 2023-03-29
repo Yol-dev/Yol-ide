25dd5796d90327a1a9e00941e70ef67bd9c14742
LixagDev <valent1.arn@gmail.com>
2023-03-29 15:20:41 +0200

    Uptade Laravel

---
+++ yolide laravel/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page Home</title>
+</head>
+<body>
+    <h1>Page Home Yol.Ide</h1>
+</body>
+</html>
--- yolide laravel/routes/web.php
+++ yolide laravel/routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
