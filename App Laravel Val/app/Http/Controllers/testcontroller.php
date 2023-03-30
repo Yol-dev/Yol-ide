b50be67d8c5b6b4fadcc27b92b13dfcea7ce742f
LixagDev <valent1.arn@gmail.com>
2023-03-30 09:51:30 +0200

    Update testcontroller.php

--- App Laravel Val/app/Http/Controllers/testcontroller.php
+++ App Laravel Val/app/Http/Controllers/testcontroller.php
@@ -9,7 +9,8 @@ class testcontroller extends Controller
     public function index()
     {
         $test = "je suis la variable test";
+        $test2 = "je suis le second";
 
-        return view('test', compact('test'));
+        return view('test', compact('test', 'test2'));
     }
 }
